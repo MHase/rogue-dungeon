0ec4fa74f021e26f40a10917f1b877686b7aa3f2
Maciej Sykuła <maciek.sykula@gmail.com>
2018-03-15 18:49:17 +0100

    Add bullets class for player to shoot

---
+++ src/bullet.js
@@ -0,0 +1,40 @@
+class Bullet extends Phaser.GameObjects.Text {
+  constructor(scene) {
+    super(scene);
+    this.scene = scene;
+    Phaser.GameObjects.Text.call(this, scene, 0, 0, 'pium pium', {
+      font: '5px Viga',
+      color: 'white',
+    });
+    scene.physics.world.enable(this);
+  }
+
+  fire(gun) {
+    this.speed = 300;
+
+    this.setActive(true);
+    this.setVisible(true);
+    this.rotation = gun.rotation;
+
+    const width = 10;
+    const offset = new Phaser.Geom.Point(width, 0);
+    Phaser.Math.Rotate(offset, gun.rotation);
+    this.setPosition(gun.x + offset.x, gun.y + offset.y);
+
+    const angle = Phaser.Math.DegToRad(gun.body.rotation);
+    this.scene.physics.velocityFromRotation(angle, this.speed, this.body.velocity);
+  }
+
+  hide() {
+    this.setActive(false);
+    this.setVisible(false);
+    this.body.stop();
+  }
+
+  hit() {
+    console.log('boom!');
+    this.hide();
+  }
+}
+
+export default Bullet;
--- src/player.js
+++ src/player.js
@@ -1,3 +1,5 @@
+import Bullet from './bullet';
+
 class Player extends Phaser.GameObjects.Sprite {
   constructor(config) {
     super(config.scene, config.x, config.y, config.key);
@@ -5,40 +7,90 @@ class Player extends Phaser.GameObjects.Sprite {
     this.key = config.key;
     config.scene.physics.world.enable(this);
     this.scene.add.existing(this);
-    // console.log(this);
+
+    this.life = 100;
+    this.cameraCenter = {
+      y: this.scene.cameras.main.height / 2,
+      x: this.scene.cameras.main.width / 2,
+    };
     this.create();
   }
 
   create() {
     this.body.setCollideWorldBounds(true);
-    // this.scene.cameras.main.startFollow(this);
+    this.scene.cameras.main.startFollow(this);
+    this.scene.cameras.main.zoom = 2;
+    this.scene.input.scale = 0.5;
+
+    this.graphics = this.scene.add.graphics({ lineStyle: { width: 1, color: 0xaa00aa } });
+    this.line = new Phaser.Geom.Line(this.y, this.x);
+
+    this.bullets = this.scene.physics.add.group({
+      classType: Bullet,
+      maxSize: 20,
+      runChildUpdate: true,
+    });
+
+    this.scene.input.on('pointermove', (pointer) => {
+      // const angle = ((Math.atan2(pointer.y - this.y, pointer.x - this.x) * 180) / Math.PI);
+      const angle = ((Math.atan2(pointer.y - this.cameraCenter.y, pointer.x - this.cameraCenter.x) * 180) / Math.PI); // my player change position all the time with movement, so center of caemra is the same positiosn ans player's
+      this.angle = parseInt(angle, 10);
+    });
+
+    this.scene.input.on('pointerdown', () => {
+      const bullet = this.bullets.get();
+
+      if (bullet) {
+        // const offset = new Phaser.Geom.Point(0, -this.height / 2);
+        const offset = 0;
+        Phaser.Math.Rotate(offset, this.angle);
+        bullet.fire(this);
+      }
+    });
   }
 
   update(keys) {
+    this.movement(keys);
+    this.animations();
+    this.redraw(this.line);
+    this.scene.physics.add.collider(
+      this.bullets,
+      this.scene.groundLayer,
+      this.handleBulletHit,
+      null,
+      this.scene,
+    );
+    // this.scene.physics.collide(
+    //   this.bullets,
+    //   this.scene.groundLayer,
+    //   () => this.bullets.destroy(), // handle removing
+    // );
+  }
+
+  handleBulletHit(bullet, tile) {
+    console.log(tile); // remove!
+    bullet.hit();
+  }
+
+  movement(keys) {
     if (keys.A.isDown) {
       this.body.setVelocityX(-100);
-      // this.anims.play('left', true);
     } else if (keys.D.isDown) {
       this.body.setVelocityX(100);
-      // this.anims.play('right');
     } else {
       this.body.setVelocityX(0);
-      // this.anims.stop('stand');
     }
 
     if (keys.S.isDown) {
       this.body.setVelocityY(100);
-      // this.anims.play('down');
     } else if (keys.W.isDown) {
       this.body.setVelocityY(-100);
-      // this.anims.play('up');
     } else this.body.setVelocityY(0);
+  }
 
-
-    if (this.body.velocity.x === 0 && this.body.velocity.y === 0) {
+  animations() {
+    if (this.body.velocity.x === 0 && this.body.velocity.y === 0)
       this.anims.play('stand');
-      // this.frame = 0;
-    }
     if (this.body.velocity.x > 0 && (this.body.velocity.y >= 0 || this.body.velocity.y < 0))
       this.anims.play('right', true);
     if (this.body.velocity.x < 0 && (this.body.velocity.y >= 0 || this.body.velocity.y < 0))
@@ -47,6 +99,16 @@ class Player extends Phaser.GameObjects.Sprite {
       this.anims.play('down', true);
     if (this.body.velocity.x === 0 && this.body.velocity.y < 0)
       this.anims.play('up', true);
+  }
+
+  redraw(line) {
+    this.line.x1 = this.x;
+    this.line.y1 = this.y;
+    this.line.x2 = this.scene.input.activePointer.worldX;
+    this.line.y2 = this.scene.input.activePointer.position.y;
+
+    this.graphics.clear();
+    this.graphics.strokeLineShape(line);
   }
 }
 
