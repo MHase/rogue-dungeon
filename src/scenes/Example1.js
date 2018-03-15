fb956dd9e05370086427bc636afd571df4749328
Maciej Sykuła <maciek.sykula@gmail.com>
2018-03-15 18:50:19 +0100

    Remove rest of matter physics and useless lines, generate player from tilemap object

--- src/scenes/Example1.js
+++ src/scenes/Example1.js
@@ -19,22 +19,31 @@ class Example1 extends Phaser.Scene {
   }
 
   create() {
-    console.log(this);
     this.map = this.make.tilemap({ key: 'map' });
     this.tileset = this.map.addTilesetImage('tileset', 'tiles');
     this.groundLayer = this.map.createStaticLayer('ground', this.tileset, 0, 0);
     this.wallsLayer = this.map.createStaticLayer('walls', this.tileset, 0, 0);
-    this.groundLayer.setCollisionByProperty({ collide: true });
-    this.wallsLayer.setCollisionByProperty({ collide: true });
+
+    this.groundLayer.setCollision(-1); // don't collide with anything from groundLayer and will collide with any other layer
+
+    // this.groundLayer.setCollisionByProperty({ collide: true });
+    // this.wallsLayer.setCollisionByProperty({ collide: true });
     // console.log(this);
-    this.matter.world.convertTilemapLayer(this.groundLayer);
-    this.matter.world.convertTilemapLayer(this.wallsLayer);
+    // this.matter.world.convertTilemapLayer(this.groundLayer);
+    // this.matter.world.convertTilemapLayer(this.wallsLayer);
 
     // this.matter.world.setBounds(this.map.widthInPixels, this.map.heightInPixels);
     this.matter.world.createDebugGraphic();
 
-    // this.map.getObjectLayer('skull').objects.map((skull) => {
-    // });
+    this.map.getObjectLayer('player').objects.map((player) => {
+      this.player = new Player({
+        scene: this,
+        key: 'player',
+        x: player.x,
+        y: player.y,
+      });
+      return null;
+    });
 
     makeAnimations(this);
 
@@ -46,18 +55,6 @@ class Example1 extends Phaser.Scene {
       W: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W),
     };
 
-    // changing scenes
-    this.input.keyboard.on('keyup', (e) => {
-      e.key === '2' && this.scene.start('Example2');
-      e.key === '3' && this.scene.start('Example3');
-    });
-
-    this.player = new Player({
-      scene: this,
-      key: 'player',
-      x: this.sys.game.config.width / 2,
-      y: (this.sys.game.config.height / 2) - 150,
-    });
     this.physics.add.collider(this.player, this.groundLayer);
     this.physics.add.collider(this.player, this.wallsLayer);
   }
