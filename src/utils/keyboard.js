3bcfcbc88177d3b80eb7b4ed47d11c7924fd90ca
Maciej Sykuła <maciek.sykula@gmail.com>
2018-04-06 10:12:14 +0200

    Change player controls to arrows

--- src/utils/keyboard.js
+++ src/utils/keyboard.js
@@ -3,26 +3,31 @@ import game from '../main';
 class PlayerKeyboard {
   constructor(player) {
     this.player = player;
-    this.keys = {
-      A: game.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A),
-      S: game.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S),
-      D: game.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D),
-      W: game.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W),
-    };
+    // this.keys = {
+    //   A: game.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A),
+    //   S: game.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S),
+    //   D: game.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D),
+    //   W: game.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W),
+    // };
+    this.cursors = game.input.keyboard.createCursorKeys();
   }
 
   update() {
-    if (this.keys.A.isDown) {
+    if (this.cursors.left.isDown) {
+    // if (this.keys.A.isDown) {
       this.player.body.setVelocityX(-100);
-    } else if (this.keys.D.isDown) {
+    } else if (this.cursors.right.isDown) {
+    // } else if (this.keys.D.isDown) {
       this.player.body.setVelocityX(100);
     } else {
       this.player.body.setVelocityX(0);
     }
 
-    if (this.keys.S.isDown) {
+    if (this.cursors.down.isDown) {
+    // if (this.keys.S.isDown) {
       this.player.body.setVelocityY(100);
-    } else if (this.keys.W.isDown) {
+    } else if (this.cursors.up.isDown) {
+    // } else if (this.keys.W.isDown) {
       this.player.body.setVelocityY(-100);
     } else this.player.body.setVelocityY(0);
   }
